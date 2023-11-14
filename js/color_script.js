911bd9472cac39300835cdbce4c935bf8ca08444
Bpadilla18 <bpadilla16@cps.edu>
2023-11-13 21:18:22 -0600

    update

--- js/color_script.js
+++ js/color_script.js
@@ -1,1 +1,13 @@
-
+function init (){
+  var el = document.getElementByID('canvas');
+  var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+  var mapOptions = {
+    center: mylocation,
+	zoom:18,
+	mapTypeId: google.maps.MapTypeId.SATELLITE,
+	mapTypeControlOptions: {
+	  position: google.maps.ContolPosition.BOTTOM_CENTER
+	}
+  };
+  
+ google.maps.event.addDomListener(window, 'load', init);
