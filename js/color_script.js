75a6380694fb96a9e82ac7bb814f8d66a3d48d45
Bpadilla18 <bpadilla16@cps.edu>
2023-11-30 10:37:32 -0600

    update

--- js/color_script.js
+++ js/color_script.js
@@ -1,39 +1,33 @@
-var map;
+
 function initMap() {
 
-  var map = new google.maps.Map(document.getElementById('canvas'), {
+  const map = new google.maps.Map(document.getElementById('canvas'), {
     center: {lat: 35.68683092590189, lng: 139.75276691412049},
 	zoom: 11
   });
 
-  var Palace = new google.maps.Marker({
+  const Palace = new google.maps.Marker({
     position: {lat: 35.68683092590189, lng: 139.75276691412049},
 	map: map
   });
 
-  var Skytree = new google.maps.Marker({
+  const Skytree = new google.maps.Marker({
     position: {lat: 35.770654805822694, lng: 139.7975586080468}, 
 	map: map
   });
 
-  var Crossing = new google.maps.Marker({
+  const Crossing = new google.maps.Marker({
 	position: {lat: 35.65957633268136, lng: 139.70052411989343}, 
 	map: map
   });
   }
   
-  var map = new google.maps.Map(el, mapOptions);
+  const map = new google.maps.Map(el, mapOptions);
   
-var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-  var marker = new google.maps.Marker({
-    position: {lat: 35.68683092590189, lng: 139.75276691412049},
-    map: map,
-    icon: iconBase + 'parking_lot_maps.png'
-  });
 	
-  var contentString = '<h2>The Imperial Palace</h2>';
+  const contentString = '<h2>The Imperial Palace</h2>';
 
-  var infowindow = new google.maps.InfoWindow({
+  const infowindow = new google.maps.InfoWindow({
     content: contnetString,
 	ariaLable: "Palace"
   });
@@ -41,5 +35,15 @@ var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.open(myMap, Marker);
   });
+  
+  const marker = new google.maps.Marker({
+    position: {lat: 35.68683092590189, lng: 139.75276691412049},
+    map,
+    title: "Imperial Palace",
+	icon: {
+    url:"media/Tokyo-imperial-palace.png",
+	scaledSize: new google.maps.Size(86,80),
+	},
+   });
 
 google.maps.event.addDomListener(window, 'load', init);
